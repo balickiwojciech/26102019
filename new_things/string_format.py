6c507a8daa47abbea64c12581ed62b951070557a
Wojtek <balickiwojciech1@gmail.com>
2019-10-26 11:15:53 +0200

    Dodaliśmy plik new_thing do GITa

---
+++ new_things/string_format.py
@@ -0,0 +1,15 @@
+# x = 5
+# krotka = ("element", "drugi_element")
+#
+# print(f"nasza liczba to {x + 10} blabla balal {krotka}")
+# print(f"nasza liczba to", x)
+# print("nasza liczba to {} blabla balal {}".format(x+15, krotka))
+#
+# ciag_znakow = "nasza liczba to {} blabla balal {}"
+#
+# print(ciag_znakow.format(x+15, krotka))
+
+Name = input("wprowadź imie:")
+Nazwisko = input("wprowadź nazwisko:")
+
+print("Twoje Imie to {}, a nazwisko to {}".format(Name.capitalize(), Nazwisko.capitalize()))
